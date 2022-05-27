7827902d6c06c27ff62e73ddf4ab2bdd15ef432a
ascd135 <74607752+ascd135@users.noreply.github.com>
2022-05-27 15:23:08 +0900

    MongoDB connection

--- server/app.js
+++ server/app.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import config from './config';
 
 const app = express();
+const {MONGO_URI} = config;
+
+
+// 몽고db연결
+mongoose.connect(MONGO_URI, {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+}).then(()=> console.log("MongoDB connectiong Success!!")).catch((e)=> console.log(e));
 
 // 처음에 신호 들어온것을 모두 받아들여라
 app.get('/');
+
 
 // 모듈화 시켜서 다른파일에서 사용할수 있게된다.
 export default app;
